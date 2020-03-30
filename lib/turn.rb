d5d84230e7801e1b1b382a875d480da366ab90cb
Harish Manoharan <harishmanoharan17@gmail.com>
2020-03-30 09:58:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,19 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board,index)
+  if index>=0 && index<=8
+    if !position_taken?(board,index)
+      return true
+    else
+      return false
+    end
+  else
+    return false
+  end
+end
+
+def position_taken?(board,index)
+  if(board[index]==" ")
+    
